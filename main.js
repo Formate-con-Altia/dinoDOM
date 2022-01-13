867d58363aabe531bd560f23b07f2c27ff4ace41
Oscar Miras <oscar.miras@gmail.com>
2022-01-13 10:40:19 +0100

    api async modulos

---
+++ main.js
@@ -0,0 +1,10 @@
+// Script.js
+
+crearDinoSeto();
+
+// Cada 5 segundos me gustaría que el título del dinoDom cambie de color. Cada 5 segundos tengo que ejecutar la función cambiarColorTituloAleatorio
+
+const myTimeout = setInterval(cambiarColorTituloAleatorio, 5000);
+
+// Obtener un botón que cada vez que lo pulsemos obtengamos una chiste de la API de chuck Norris jokes y cuadro de texto
+
---
+++ utils.js
@@ -0,0 +1,6 @@
+function colorHexadecimalAleatorio() {
+    let color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart('6', '0');
+
+    return color;
+}
+
